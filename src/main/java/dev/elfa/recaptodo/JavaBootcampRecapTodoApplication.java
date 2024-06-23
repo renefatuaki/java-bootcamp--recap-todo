487a862ae08a12c5fdcf2ae773745f445343bc6a
René El Fatuaki <rene@elfatuaki.com>
2024-06-23 18:49:23 +0200

    Initial commit

---
+++ src/main/java/dev/elfa/recaptodo/JavaBootcampRecapTodoApplication.java
@@ -0,0 +1,13 @@
+package dev.elfa.recaptodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBootcampRecapTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBootcampRecapTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/elfa/recaptodo/JavaBootcampRecapTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elfa.recaptodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBootcampRecapTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
