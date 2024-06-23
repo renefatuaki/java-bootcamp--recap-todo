7568a709dd106e62145fdd9b6ba69c742dddd1ee
René El Fatuaki <rene@elfatuaki.com>
2024-06-23 18:57:46 +0200

    Added static files for frontend

--- src/main/java/dev/elfa/recaptodo/JavaBootcampRecapTodoApplication.java
+++ src/main/java/dev/elfa/recaptodo/JavaBootcampRecapTodoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaBootcampRecapTodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaBootcampRecapTodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBootcampRecapTodoApplication.class, args);
+    }
 
 }
